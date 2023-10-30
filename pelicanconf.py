edf135f60ee5ff558d016929b7e80e6afd73d95e
Ivan Marin <ivan@tunguska.cc>
2023-10-29 21:49:11 -0300

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+AUTHOR = 'Ivan M'
+SITENAME = 'Tunguska Labs'
+SITEURL = 'https://labs.tunguska.cc'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Tunguska Labs', '#'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/ispmarin'),
+          ('Linkedin', 'https://linkedin.com/in/ispmarin'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME='cid'
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
